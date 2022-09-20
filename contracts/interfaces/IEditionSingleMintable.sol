71e42e87dfa74d85ef4fadcecc15da11e586695b
karmacoma <karma@coma.lol>
2022-09-19 17:33:22 -0700

    Complete interface IEditionSingleMintable.sol

--- contracts/interfaces/IEditionSingleMintable.sol
+++ contracts/interfaces/IEditionSingleMintable.sol
@@ -2,9 +2,56 @@
 pragma solidity ^0.8.6;
 
 interface IEditionSingleMintable {
-  function mintEdition(address to) external returns (uint256);
-  function mintEditions(address[] memory to) external returns (uint256);
-  function maxSupply() external view returns (uint256);
-  function totalSupply() external view returns (uint256);
-  function owner() external view returns (address);
+    event EditionSold(uint256 price, address owner);
+    event PriceChanged(uint256 amount);
+
+    function burn(uint256 tokenId) external;
+
+    function description() external view returns (string memory);
+
+    function editionSize() external view returns (uint256);
+
+    function getURIs()
+        external
+        view
+        returns (
+            string memory,
+            string memory
+        );
+
+    function initialize(
+        address _owner,
+        string memory _name,
+        string memory _symbol,
+        string memory _description,
+        string memory _animationUrl,
+        string memory _imageUrl,
+        uint256 _editionSize,
+        uint256 _royaltyBPS
+    ) external;
+
+    function maxSupply() external view returns (uint256);
+
+    function mintEdition(address to) external returns (uint256);
+
+    function mintEditions(address[] memory recipients)
+        external
+        returns (uint256);
+
+    function purchase() external payable returns (uint256);
+
+    function salePrice() external view returns (uint256);
+
+    function setApprovedMinter(address minter, bool allowed) external;
+
+    function setSalePrice(uint256 _salePrice) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function updateEditionURLs(
+        string memory _imageUrl,
+        string memory _animationUrl
+    ) external;
+
+    function withdraw() external;
 }
