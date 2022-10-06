f7ebda49d25f0f80e1021dc0c30add550bdaf5bf
karmacoma <karma@coma.lol>
2022-10-06 13:54:22 -0700

    Add testToken/ContractURIEscaped in EditionMetadataTest

--- test/EditionMetadataTest.sol
+++ test/EditionMetadataTest.sol
@@ -9,27 +9,45 @@ import {EditionCreator} from "../contracts/EditionCreator.sol";
 contract EditionMetadataTest is Test {
     EditionCreator editionCreator;
     Edition edition;
+    Edition editionToEscape;
     uint256 constant tokenId = 1;
+
+    function createEdition(string memory name, string memory description)
+        internal
+        returns (Edition)
+    {
+        return
+            Edition(
+                address(
+                    editionCreator.createEdition(
+                        name,
+                        "TEST",
+                        description,
+                        "https://example.com/animation.mp4",
+                        "https://example.com/image.png",
+                        10, // editionSize
+                        10_00, // royaltyBPS
+                        0 // metadataGracePeriodSeconds
+                    )
+                )
+            );
+    }
 
     function setUp() public {
         editionCreator = new EditionCreator(address(new Edition()));
 
-        edition = Edition(
-            address(
-                editionCreator.createEdition(
-                    "This is the name of my edition",
-                    "TEST",
-                    "This is a short description.",
-                    "https://example.com/animation.mp4",
-                    "https://example.com/image.png",
-                    10, // editionSize
-                    10_00, // royaltyBPS
-                    0 // metadataGracePeriodSeconds
-                )
-            )
+        edition = createEdition(
+            "This is the name of my edition",
+            "This is a short description."
+        );
+
+        editionToEscape = createEdition(
+            'My "edition" is \t very special!\n',
+            'My "description" is also \t \\very\\ special!\r\n'
         );
 
         edition.mintEdition(address(0xdEaD));
+        editionToEscape.mintEdition(address(0xdEaD));
     }
 
     // for gas usage only
@@ -40,5 +58,13 @@ contract EditionMetadataTest is Test {
     // for gas usage only
     function testContractURI() public {
         edition.contractURI();
+    }
+
+    function testTokenURIEscaped() public {
+        editionToEscape.tokenURI(tokenId);
+    }
+
+    function testContractURIEscaped() public {
+        editionToEscape.contractURI();
     }
 }
