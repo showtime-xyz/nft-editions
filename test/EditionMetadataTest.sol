f3e26d1b8020ee49fd7156632af103eb37b0cadd
karmacoma <karma@coma.lol>
2022-10-06 17:41:22 -0700

    Add INTENSE tests

--- test/EditionMetadataTest.sol
+++ test/EditionMetadataTest.sol
@@ -2,14 +2,22 @@
 pragma solidity ^0.8.15;
 
 import {Test} from "forge-std/Test.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {console2} from "forge-std/console2.sol";
 
 import {Edition} from "../contracts/Edition.sol";
 import {EditionCreator} from "../contracts/EditionCreator.sol";
+import {Base64} from "../contracts/utils/Base64.sol";
+import {LibString} from "../contracts/utils/LibString.sol";
 
 contract EditionMetadataTest is Test {
+    uint256 constant INTENSE_LENGTH = 100_000;
+
     EditionCreator editionCreator;
     Edition edition;
     Edition editionToEscape;
+    Edition editionIntense;
+
     uint256 constant tokenId = 1;
 
     function createEdition(string memory name, string memory description)
@@ -46,6 +54,15 @@ contract EditionMetadataTest is Test {
             'My "description" is also \t \\very\\ special!\r\n'
         );
 
+        string memory intenseDescription = LibString.repeat(
+            "\\",
+            INTENSE_LENGTH
+        );
+        editionIntense = createEdition(
+            "This edition goes to 11",
+            intenseDescription
+        );
+
         edition.mintEdition(address(0xdEaD));
         editionToEscape.mintEdition(address(0xdEaD));
     }
@@ -66,5 +83,42 @@ contract EditionMetadataTest is Test {
 
     function testContractURIEscaped() public {
         editionToEscape.contractURI();
+    }
+
+    function testLongEscapeDoubleQuotes() public {
+        string memory input = LibString.repeat('"', INTENSE_LENGTH);
+        string memory expected = LibString.repeat('\\"', INTENSE_LENGTH);
+        string memory actual = LibString.escapeJSON(input);
+        assertEq(actual, expected);
+    }
+
+    function testLongEscapeControlChar() public {
+        string memory input = LibString.repeat(
+            string(abi.encodePacked(bytes1(0))),
+            INTENSE_LENGTH
+        );
+        string memory expected = LibString.repeat("\\u0000", INTENSE_LENGTH);
+        string memory actual = LibString.escapeJSON(input);
+        assertEq(actual, expected);
+    }
+
+    function testEncodeContractURIIntenseDescription() public {
+        string memory contractURI = editionIntense.contractURI();
+        string memory base64Slice = LibString.slice(
+            contractURI,
+            29, // length of 'data:application/json;base64,'
+            bytes(contractURI).length
+        );
+
+        // console2.log("base64Slice:", base64Slice);
+        string memory base64Decoded = string(Base64.decode(base64Slice));
+        // console2.log("base64Decoded:", base64Decoded);
+        string memory description = abi.decode(
+            stdJson.parseRaw(base64Decoded, ".description"),
+            (string)
+        );
+        // console2.log("description:", description);
+
+        assertEq(description, LibString.repeat("\\", INTENSE_LENGTH));
     }
 }
