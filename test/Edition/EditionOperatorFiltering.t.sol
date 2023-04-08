d74cffef1f73e28adf492a66cb217cead48262e5
karmacoma <karma@coma.lol>
2023-04-08 16:45:35 -0700

    Implement operator filtering in SingleBatchEdition
    + make the base spec implement EditionOperatorFiltering, so that all edition implementations get tested for operator filtering

--- test/Edition/EditionOperatorFiltering.t.sol
+++ test/Edition/EditionOperatorFiltering.t.sol
@@ -30,6 +30,8 @@ contract MockRegistry {
 
 /// @dev modified from operator-filter-registry/test/Validation.t.sol
 abstract contract EditionOperatorFiltering is EditionFixture {
+    using EditionConfigWither for EditionConfig;
+
     address constant CANONICAL_OPERATOR_FILTER_REGISTRY = 0x000000000000AAeB6D7670E522A718067333cd4E;
     address constant CANONICAL_OPENSEA_REGISTRANT = 0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6;
 
@@ -46,17 +48,10 @@ abstract contract EditionOperatorFiltering is EditionFixture {
 
     address[] filteredOperators;
 
-    // implementation must initialize the __filtering_* variables
-    function __EditionOperatorFiltering_init() internal virtual;
-
-    // implementation must provide a way to mint a token
-    function __EditionOperatorFiltering_mint(address someEdition, address recipient)
-        internal
-        virtual
-        returns (uint256 token);
-
     // implementation must invoke this after init()
-    function __EditionOperatorFiltering_setUp() public {
+    function setUp() public virtual {
+        __filtering_edition = EditionBase(create(DEFAULT_CONFIG.withName("Operator Filtering Test")));
+
         // try to load contract address from .env
         try vm.envAddress("CONTRACT_ADDRESS") returns (address _contractAddress) {
             contractAddress = _contractAddress;
@@ -72,7 +67,7 @@ abstract contract EditionOperatorFiltering is EditionFixture {
             __filtering_tokenId = _tokenId;
         } catch (bytes memory) {
             // fallback to minting
-            __filtering_tokenId = __EditionOperatorFiltering_mint(address(__filtering_edition), address(this));
+            __filtering_tokenId = mint(address(__filtering_edition), address(this));
         }
 
         // try to load owner from .env
@@ -95,7 +90,7 @@ abstract contract EditionOperatorFiltering is EditionFixture {
             );
         } catch {
             // fallback to static list
-            console2.log("No network specified, using static list (Dec 2022 snapshot)");
+            // console2.log("No network specified, using static list (Dec 2022 snapshot)");
 
             filteredOperators = abi.decode(
                 hex"0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000111abe46ff893f3b2fdf1f759a8a8000000000000000000000000fed24ec7e22f573c2e08aef55aa6797ca2b3a051000000000000000000000000f42aa99f011a1fa7cda90e5e98b277e306bca83e000000000000000000000000b16c1342e617a5b6e4b631eb114483fdb289c0a4000000000000000000000000d42638863462d2f21bb7d4275d7637ee5d5541eb00000000000000000000000008ce97807a81896e85841d74fb7e7b065ab3ef0500000000000000000000000092de3a1511ef22abcf3526c302159882a4755b22000000000000000000000000cd80c916b1194beb48abf007d0b79a7238436d56",
@@ -108,19 +103,46 @@ abstract contract EditionOperatorFiltering is EditionFixture {
         }
     }
 
-    function testEnableDefaultOperator() public {
-        vm.startPrank(editionOwner);
+    function test_setOperatorFilter_onlyOwnerFail(address passerBy) public {
+        vm.assume(passerBy != editionOwner);
 
-        __filtering_edition.setOperatorFilter(address(0));
-        assertEq(__filtering_edition.activeOperatorFilter(), address(0));
-
-        __filtering_edition.enableDefaultOperatorFilter();
-        assertEq(__filtering_edition.activeOperatorFilter(), CANONICAL_OPENSEA_REGISTRANT);
-
-        vm.stopPrank();
+        // when somebody else tries to set an operator filter, it reverts
+        vm.expectRevert("UNAUTHORIZED");
+        vm.prank(passerBy);
+        __filtering_edition.setOperatorFilter(passerBy);
     }
 
-    function testOperatorFiltering() public {
+    function test_setOperatorFilter_onlyOwnerPass(address newFilter) public {
+        // when the owner sets an operator filter
+        vm.prank(editionOwner);
+        __filtering_edition.setOperatorFilter(newFilter);
+
+        // then the operator filter is set
+        assertEq(__filtering_edition.activeOperatorFilter(), newFilter);
+    }
+
+    function test_enableDefaultOperatorFilter_onlyOwnerFail(address passerBy) public {
+        vm.assume(passerBy != editionOwner);
+
+        // when somebody else tries to enable the default operator filter, it reverts
+        vm.expectRevert("UNAUTHORIZED");
+        vm.prank(passerBy);
+        __filtering_edition.enableDefaultOperatorFilter();
+    }
+
+    function test_enableDefaultOperatorFilter_onlyOwnerPass() public {
+        // when the owner enables the default operator filter
+        vm.prank(editionOwner);
+        __filtering_edition.enableDefaultOperatorFilter();
+
+        // then the operator filter is set
+        assertEq(
+            __filtering_edition.activeOperatorFilter(),
+            address(__filtering_edition.CANONICAL_OPENSEA_SUBSCRIPTION())
+        );
+    }
+
+    function test_e2e_operatorFiltering() public {
         IERC721 nftContract = IERC721(contractAddress);
 
         // Try to get the current owner of the NFT, falling back to value set during setup on revert
@@ -132,7 +154,7 @@ abstract contract EditionOperatorFiltering is EditionFixture {
 
         for (uint256 i = 0; i < filteredOperators.length; i++) {
             address operator = filteredOperators[i];
-            console2.log("Testing operator: ", operator);
+            console2.log("Testing operator:", operator);
 
             // Try to set approval for the operator
             vm.startPrank(__filtering_owner);
@@ -150,8 +172,8 @@ abstract contract EditionOperatorFiltering is EditionFixture {
                 // continue to test transfer methods, since marketplace approvals can be
                 // hard-coded into contracts
             }
-
             vm.stopPrank();
+
             // Ensure operator is not able to transfer the token
             vm.startPrank(operator);
             vm.expectRevert();
